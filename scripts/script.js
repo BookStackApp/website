0a70a977556ac9461624a64d7c2c9b9fade28019
Dan Brown <ssddanbrown@googlemail.com>
2016-03-08 21:09:07 +0000

    Trimmed page content down, Removed annoying animations and added blog link

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,4 @@
 
-var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'anim', // animation css class (default is animated)
-    offset:       80,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    scrollContainer: null // optional scroll container selector, otherwise use window
-  }
-);
-wow.init();
-
-
 var initPhotoSwipeFromDOM = function(gallerySelector) {
 
     // parse slide data (url, title, size ...) from DOM elements 

42a68a7e9c1819d5e0c110d1c19aaad9fbb28f0f
Pascal Borreli <pascal@borreli.com>
2016-03-24 19:20:05 +0000

    Fixed typos

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ function icon($name) {
     return (file_get_contents('icons/' . $name . '.svg'));
 }
 
-// Helper to link to a documenation page
+// Helper to link to a documentation page
 function docLink($page) {
 	return '/docs/' . $page;
 }
